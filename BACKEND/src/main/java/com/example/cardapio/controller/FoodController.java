cf58d6cfc0a324b95076f40132023fcbfca09513
Adailson <adayllson.almeida@gmail.com>
2023-10-27 15:40:39 -0300

    configurando a requestDTO

--- BACKEND/src/main/java/com/example/cardapio/controller/FoodController.java
+++ BACKEND/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,16 +1,20 @@
 package com.example.cardapio.controller;
 
-import java.util.List;
+//import java.util.List;
 import java.util.stream.Stream;
 
 //import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cardapio.Food.Food;
 //import com.example.cardapio.Food.Food;
 import com.example.cardapio.Food.FoodRepository;
+import com.example.cardapio.Food.FoodRequestDTO;
 import com.example.cardapio.Food.FoodResponseDTO;
 
 @RestController
@@ -19,12 +23,19 @@ public class FoodController {
 
     @Autowired
     private FoodRepository repository;
+
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food fooData = new Food(data);
+        repository.save(fooData);
+        return;
+    }
     
     @GetMapping
-    public List<FoodResponseDTO> getAll(){
+    public Stream<Object> getAll(){
 
         Stream<Object> foodList = repository.findAll().stream().map(FoodResponseDTO::new);
-        return (List<FoodResponseDTO>) foodList;
+        return foodList;
     }
     
 }
