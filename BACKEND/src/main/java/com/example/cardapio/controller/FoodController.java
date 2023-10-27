3b131b04c7077aedce9e23080a39bc02a6fa8c8f
Adailson <adayllson.almeida@gmail.com>
2023-10-27 11:52:56 -0300

    criaçao da classe food

--- BACKEND/src/main/java/com/example/cardapio/controller/FoodController.java
+++ BACKEND/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,16 +1,28 @@
 package com.example.cardapio.controller;
 
+import java.util.List;
+
+//import org.hibernate.mapping.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cardapio.Food.Food;
+import com.example.cardapio.Food.FoodRepository;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
 
+    @Autowired
+    private FoodRepository repository;
+    
     @GetMapping
-    public void getAll(){
-        
+    public List<Food> getAll(){
+
+        List<Food> foodList = repository.findAll();
+        return foodList;
     }
     
 }
