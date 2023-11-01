fad3fe2244f2747f4815eec64513114b0a82da3c
Adailson <adayllson.almeida@gmail.com>
2023-11-01 16:09:53 -0300

    Fazendo com que a modal apareça na tela

--- FRONTEND/cardapio/src/App.tsx
+++ FRONTEND/cardapio/src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './App.css'
 import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
   const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
     <div className="container">
@@ -13,7 +20,10 @@ function App() {
           price={foodData.price} 
           title={foodData.title} 
           image={foodData.image}/>)}
+          
       </div>
+      {isModalOpen && <CreateModal/>}
+      <button onClick={handleOpenModal}>Novo</button>
     </div>
   )
 }
