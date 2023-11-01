9bccbb18c8670932e7940c01bbe5c8d20ead4c14
Adailson <adayllson.almeida@gmail.com>
2023-11-01 16:04:18 -0300

    Criando as funções de post do produto

--- FRONTEND/cardapio/src/components/create-modal/create-modal.tsx
+++ FRONTEND/cardapio/src/components/create-modal/create-modal.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
+import { useFoodDataMutate } from "../../hooks/useFoodDataMutate";
+import { FoodData } from "../../inteface/FoodData";
+
 interface InputProps {
     label: string,
     value: string | number,
-    updateValue(value: string | number): void
+    updateValue(value: any): void
 }
 
 const Input = ({label, value, updateValue}: InputProps) => {
     return(
         <>
             <label>{label}</label>
-            <input value={value} onChange={e => updateValue(e.target.value)}></input>
+            <input value={value} onChange={event => updateValue(event.target.value)}></input>
         </>
     )
 }
@@ -16,14 +20,34 @@ const Input = ({label, value, updateValue}: InputProps) => {
 
 
 export function CreateModal(){
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [image, setImage] = useState("");
+    const {mutate} = useFoodDataMutate();
+
+    const submit = () => {
+        const foodData: FoodData = {
+            title,
+            price,
+            image
+            
+
+    }
+    mutate(foodData);
+
+}
+    
     return(
         <div className="modal-overlay">
             <div className="modal-body">
                 <h2>Cadastre um novo item no cardápio</h2>
                 <form className="input-container">
-
+                    <Input label="title" value={title} updateValue={setTitle} />
+                    <Input label="price" value={price} updateValue={setPrice} />
+                    <Input label="image" value={image} updateValue={setImage} />
 
                 </form>
+                <button onClick={submit} className="btn-secondary">Cadastrar</button>
             </div>
         </div>
     )
